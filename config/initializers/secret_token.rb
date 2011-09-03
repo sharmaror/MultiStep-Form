2d54797decb79f11df08c2e4ff54644489e8581e
gopi krishna <gopi.ror@live.com>
2011-09-03 21:28:56 +0530

    commit for my first app in heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multipleforms::Application.config.secret_token = '92f01dc899682934fc78f0c0fedaed1701545ff7169e531245f06c76aada5b5cc42491ec7bbfcd16f6c8bcbf85b9d0ffe9dfba6224d15edb1a02ff8afe09249e'
